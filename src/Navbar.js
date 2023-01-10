9087afe9878e02216af24c9737e84addf7f22edd
Maniac <drno3171@gmail.com>
2023-01-10 23:57:09 +0530

    logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,12 +5,15 @@ import App from './App';
 import Page1 from './page1';
 import { Outlet } from 'react-router-dom';
 import './navbar.css';
-
+import logo from './logo192.png'
 function Navbar() {
   return (
      
     <nav className='navbar '>
+		<ul>
+		<a href="/" className='site-title '> <img src={logo} alt={"logo"} width={40} height={40} /></a>
         <a href="/" className='site-title '>Home</a>
+		</ul>
         <ul>
 			<li><a href="/page1">Page-1</a></li>
         	<li><a href="/page2">Page-2</a></li>
